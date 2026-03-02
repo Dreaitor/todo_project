27d17cc8988aed4a0badb9af51c2ebd30d45522c
10554 <1055491796@qq.com>
2026-03-02 10:49:49 +0800

    新建项目

---
+++ src/main/java/com/zhang/todo_project_server/TodoProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.todo_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoProjectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhang/todo_project_server/TodoProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.todo_project_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoProjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
